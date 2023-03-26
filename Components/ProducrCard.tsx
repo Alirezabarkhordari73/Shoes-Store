6f721909eecdfaf920db309fd29df06099f694c3
Alireza Barkhordari <aliirezaa.barkhordarii@gmail.com>
2023-03-26 22:37:56 +0430

    some change to product card

--- Components/ProducrCard.tsx
+++ Components/ProducrCard.tsx
@@ -6,10 +6,10 @@ const ProductCard = () => {
   return (
     <Link
       href={`/product`}
-      className="transform overflow-hidden relative bg-white duration-500 transition-all hover:scale-105 cursor-pointer"
+      className="transform overflow-hidden relative bg-white duration-300 transition-all hover:scale-105 cursor-pointer"
     >
       <Image width={380} height={380} src="/slide-8.webp" alt="" />
-      <div className="mt-3 flex flex-col justify-start">
+      <div className="mt-3 flex flex-col justify-start px-2">
         <div className="flex justify-between items-start">
           <h1 className="text-[0.9rem] text-gray-400">نام محصول</h1>
           <StarRate Ratevalue={4} />
@@ -20,11 +20,21 @@ const ProductCard = () => {
             نگه داریم و همیشه در دسترس شما قرار دهیم
           </p>
         </div>
-        <div className="flex gap-2 mt-2">
-          <h1 className="text-[#66b3e6] text-[1.1rem] font-semibold tracking-[1px]">
-            3200
-          </h1>
-          <h1 className="text-[#66b3e6] text-[1.1rem] font-semibold">تومان</h1>
+        <div className="flex justify-between items-start">
+          <div className="flex gap-2 mt-2">
+            <h1 className="text-[#66b3e6] text-[1.1rem] font-semibold tracking-[1px]">
+              3200
+            </h1>
+            <h1 className="text-[#66b3e6] text-[1.1rem] font-semibold">
+              تومان
+            </h1>
+          </div>
+          <div className="flex gap-2 mt-2">
+            <h1 className="text-[#66b3e6] text-[1.1rem] font-semibold">10%</h1>
+            <h1 className="text-[#66b3e6] text-[1.1rem] font-semibold tracking-[1px]">
+              تخفیف
+            </h1>
+          </div>
         </div>
       </div>
       <div className="w-14 h-14 rounded-full bg-[#66b3e6] cursor-default absolute top-3 left-3 flex justify-center items-center text-white">
